9c1dbccdbfcb81aed69d713433c8172fb1d725b3
svk183@gmail.com <Mypswrd_183>
2017-10-09 13:14:14 +0530

    space shooter code for video 1

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public GameObject projectile;
+
+    public float playerSpeed = 0.5f;
+    public float ProjectileSpeed = 0.5f;
+    public bool isShooting = true;
+
+    float maximumXPosition = 25.0f;
+
+	void Start () {
+        StartCoroutine(projectileShooterTimer());
+	}
+	
+	void Update () {
+        updatePlayerPosition();
+        spawnProjectile();
+    }
+
+    void updatePlayerPosition()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -maximumXPosition)
+            transform.Translate(-playerSpeed, 0, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < maximumXPosition)
+            transform.Translate(playerSpeed, 0, 0);
+    }
+
+    IEnumerator projectileShooterTimer()
+    {
+        if (isShooting)
+        {
+            yield return new WaitForSeconds(ProjectileSpeed);
+        }
+    }
+
+    void spawnProjectile()
+    {
+        Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/ProjectleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectleScript : MonoBehaviour {
+    public float projectileSpeed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, projectileSpeed, 0);
+	}
+}
